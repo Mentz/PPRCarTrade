fe361fbd694332eaf06b1e476150b9bdc7ac4f54
kanchii <weisslipe@gmail.com>
2017-06-25 17:08:10 -0300

    Eae

--- PPRCarTrade/src/pprcartrade/PPRCarTrade.java
+++ PPRCarTrade/src/pprcartrade/PPRCarTrade.java
@@ -5,6 +5,8 @@
  */
 package pprcartrade;
 
+import Telas.TelaLogin;
+
 /**
  *
  * @author Lucas
@@ -15,7 +17,9 @@ public class PPRCarTrade {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        new TelaLogin().setVisible(true);
+        
     }
     
 }
